fbcc21e21b3568621ae40b30004a9a7fb2762db3
CrisRuedaP <crisruedap@gmail.com>
2020-02-05 21:02:22 +0000

    Printed with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
+/*Printed with function puts */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
