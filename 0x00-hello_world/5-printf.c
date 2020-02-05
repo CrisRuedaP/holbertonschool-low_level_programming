0acacbe3ef23214aab6d18defe16d3e7cfc90c9f
CrisRuedaP <crisruedap@gmail.com>
2020-02-05 21:16:26 +0000

    Printed with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/** 
+ * printed with printf                                                                                                     
+ *                                                                                                                         
+ * Return: 0                                                                                               
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,");
+  return (0);
+}
