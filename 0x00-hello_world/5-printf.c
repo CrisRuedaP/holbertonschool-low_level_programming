36ccab22a7dcd884b5a9a7415ac9280af65a391a
CrisRuedaP <crisruedap@gmail.com>
2020-02-05 21:21:37 +0000

    Printed with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-/** 
- * printed with printf                                                                                                     
- *                                                                                                                         
- * Return: 0                                                                                               
+/**
+ * main - Is the principal function
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
