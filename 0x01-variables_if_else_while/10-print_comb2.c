58eaa24bfa5db5db14fa74a9648fc79e7b7c8705
CrisRuedaP <crisruedap@gmail.com>
2020-02-08 19:04:05 +0000

    Added numbers from 00 to 99

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -17,10 +17,10 @@ for (j = 48; j < 58; j++)
 putchar(i);
 putchar(j);
 if (!(i == 57 && j == 57))
-putchar(',');
-putchar(' ');
+putchar(44);
+putchar(32);
 }
 }
-putchar('\n');
+putchar(10);
 return (0);
 }
