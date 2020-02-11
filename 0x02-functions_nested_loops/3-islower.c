a083b40701f357ca5cce883e8f8040e310da557e
CrisRuedaP <crisruedap@gmail.com>
2020-02-11 18:42:54 +0000

    Printed for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,16 @@
 #include "holberton.h"
-int _islower(int c);
 /**
- * main - check the code for Holberton School students.
- *
+ * islower - checks for lowercase.
  * Return: Always 0.
  */
-int main(void)
+int _islower(int c)
 {
-int c;
-
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
+if ((c >= 97) && (c <= 122))
+{
+return (1);
+}
+else
+{
 return (0);
+}
 }
