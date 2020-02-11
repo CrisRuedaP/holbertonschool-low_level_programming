722969994291271514cb9aa0da7859a7b6b55bd8
CrisRuedaP <crisruedap@gmail.com>
2020-02-11 00:38:21 +0000

    Print strings with _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,20 @@
-#include<stdio.h>
+#include "holberton.h"
 /**
- * main - Entry point
+ * main - check the code for Holberton School students.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-puts("Holberton\n");
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 return (0);
 }
