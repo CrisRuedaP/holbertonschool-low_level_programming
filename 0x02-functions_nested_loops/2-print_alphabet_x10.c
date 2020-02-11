1e145afe1ea7ad5b03acf1f4fc28e14ba1d22ba2
CrisRuedaP <crisruedap@gmail.com>
2020-02-11 10:47:57 +0000

    Printed 10 times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
-void print_alphabet(void);
+
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet_x10(void);
 {
 int x;
 int y;
