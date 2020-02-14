71ebdb7247c1819c11f025d048c7a3f6035883c7
CrisRuedaP <crisruedap@gmail.com>
2020-02-14 02:47:46 +0000

    Printed square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * print_line - draws a straight
- * @n: - variable declaration
+ * @size: - variable declaration
  *
  * Return: Always 0
  */
@@ -10,12 +10,13 @@ void print_square(int size)
 int c;
 int k;
 
+if (size <= 0)
+_putchar('\n');
+
 for (c = 0; c < size; c++)
 {
 for (k = 0; k < size; k++)
-{
 _putchar('#');
-}
 _putchar('\n');
 }
 }
