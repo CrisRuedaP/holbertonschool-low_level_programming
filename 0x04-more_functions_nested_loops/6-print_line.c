e1e787cf5f1eff85949e338dea6961320d73e765
CrisRuedaP <crisruedap@gmail.com>
2020-02-14 01:27:02 +0000

    function draw a straight

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,18 +2,16 @@
 /**
  * print_line - draws a straight
  * @n: - variable declaration
+ *
  * Return: Always 0
  */
 void print_line(int n)
 {
 int x;
 
-for (x = 1; x >= n; x++)
-{
-if (n <= 0)
+for (x = 0; x < n; x++)
 {
 _putchar('_');
-}
 }
 _putchar('\n');
 }
