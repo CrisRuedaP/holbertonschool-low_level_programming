50094f06d1b3624b319d66fc6fd65d3dde18325c
CrisRuedaP <crisruedap@gmail.com>
2020-02-19 03:11:16 +0000

    Fixed betty styles

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,12 @@
 #include "holberton.h"
 
 /**
- * -reset_to_98 - updates pointers value to 98
+ * reset_to_98 - updates pointers value to 98
  * @n: pointers value.
  * return: none
- **/
+ */
 
 void reset_to_98(int *n)
 {
-
 *n = 98;
-
 }
