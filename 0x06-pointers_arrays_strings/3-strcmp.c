681715f44f23b372a404a04941435cf26ad33ec3
CrisRuedaP <crisruedap@gmail.com>
2020-02-22 16:05:13 +0000

    Fixed betty and correct output

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
- * _strncat - compares two strings.
- * @s1: variable
- * @s2: variable
- * Return: resulting string dest.
+ * _strcmp - compares two strings.
+ * @s1: string
+ * @s2: string
+ * Return: result of the comparison of two strings .
  */
 
 int _strcmp(char *s1, char *s2)
@@ -22,11 +22,6 @@ return (s1[i] - s2[i]);
 else if (s1[i] < s2[i])
 {
 return (s1[i] - s2[i]);
-}
-
-else
-{
-return (0);
 }
 
 i++;
