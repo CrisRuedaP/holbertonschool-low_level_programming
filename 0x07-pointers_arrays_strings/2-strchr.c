2c25434ba6250a9c1b5e2d5347673be3522e5aab
CrisRuedaP <crisruedap@gmail.com>
2020-02-24 19:14:06 +0000

    Located character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locate a character in a string.
+ * @s: string
+ * @c: character
+ * Return: a pointer or NULL.
+ */
+
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{ 
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+return ('\0');
+}
