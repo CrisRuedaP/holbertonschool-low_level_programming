aacb160bf07546922810b808e33c88d40d142d66
CrisRuedaP <crisruedap@gmail.com>
2020-02-25 01:31:49 +0000

    fixed betty style

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #define NULL 0
 /**
- * _strbprk - finds the first character in the string that matches.
+ * _strpbrk - Finds the first character in the string that matches.
  * @s: string to be scanned
  * @accept: string containing the characters
  * Return: a pointer if it matches, else return NULL
@@ -13,7 +13,7 @@ char *s1, *s2;
 
 for (s1 = s; *s1 != '\0'; ++s1)
 {
-for (s2 = accept; *s2 != '\0'; ++s2) 
+for (s2 = accept; *s2 != '\0'; ++s2)
 {
 if (*s1 == *s2)
 return (s1);
