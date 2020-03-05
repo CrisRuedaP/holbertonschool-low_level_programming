4550d7b017a0176212a5714ab4f73b0afa904111
CrisRuedaP <crisruedap@gmail.com>
2020-03-05 21:15:46 +0000

    fixed betty

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * *create_array - creates an array of chars.
- * @size: size of array.
- * @c: array of characters
- *
- * Return: a pointer to the array.
+ * *string_nconcat - concatenated two strings.
+ * @s1: string
+ * @s2: string
+ * @n: integer
+ * Return: point to a newly allocated space in memory.
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -43,7 +43,7 @@ str[i] = s1[i];
 }
 for (j = 0; j < n; j++, i++)
 {
-str[i] = s2[j]; 
+str[i] = s2[j];
 }
 str[i] = '\0';
 return (str);
