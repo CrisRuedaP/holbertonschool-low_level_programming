e9dd254e12f85a0c0cfb759f612891951008747e
CrisRuedaP <crisruedap@gmail.com>
2020-03-06 02:16:42 +0000

    fixed betty

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -4,7 +4,7 @@
 /**
  * *array_range - creates an array of integers.
  * @min: integer value
- * @ma: integer value
+ * @max: integer value
  * Return: the pointer to the newly created array.
  */
 
