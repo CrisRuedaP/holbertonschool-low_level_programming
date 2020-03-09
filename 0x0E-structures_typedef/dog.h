ae44ab6e2e92e169248c91fd4cc0217074e385a3
CrisRuedaP <crisruedap@gmail.com>
2020-03-09 21:13:03 +0000

    header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+ * struct - check the code for Holberton School students.
+ * @name - dog name
+ * @age - age dog
+ * @owner - owner dog
+ * Return: always 0.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+
+};
+
+typedef struct dog cat;
+int _putchar(char c);
+
+#endif
