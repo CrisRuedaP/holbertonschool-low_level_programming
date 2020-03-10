43111772ce5f04f34fba3a4645ea4d18e00267d8
CrisRuedaP <crisruedap@gmail.com>
2020-03-10 22:10:57 +0000

    fixed output

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,7 +3,6 @@
 /**
  * print_dog - struct dog.
  * @d: pointer to structure
- * Return: Print element or if is NULL print nill.
  */
 void print_dog(struct dog *d)
 {
@@ -13,10 +12,12 @@ return;
 if (d->name == NULL)
 printf("Name: (nil)\n");
 
+else
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+
 if (d->owner == NULL)
 printf("Owner: (nil)\n");
 
-printf("Name: %s\n", d->name);
-printf("Age: %6f\n", d->age);
 printf("Owner: %s\n", d->owner);
 }
