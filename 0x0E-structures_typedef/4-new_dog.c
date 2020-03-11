eb7b0cc17ab678466f60e01d85413656696c4a5a
CrisRuedaP <crisruedap@gmail.com>
2020-03-11 01:25:27 +0000

    fixed output

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,41 +13,41 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 int i, j, n, o;
 
-dog_t *perrito;
+dog_t *mora;
 
-perrito = malloc(sizeof(dog_t));
+mora = malloc(sizeof(dog_t));
 
-if (perrito == NULL)
+if (mora == NULL)
 return (NULL);
 
 for (n = 0; name[n] != '\0'; n++)
 {
 }
-perrito->name = malloc((n + 1) * sizeof(char));
-if (perrito->name == NULL)
+mora->name = malloc((n + 1) * sizeof(char));
+if (mora->name == NULL)
 {
-free(perrito);
+free(mora);
 return (NULL);
 }
 
 for (o = 0; owner[o] != '\0'; o++)
 {
 }
-perrito->owner = malloc((o + 1) * sizeof(char));
-if (perrito->owner == NULL)
+mora->owner = malloc((o + 1) * sizeof(char));
+if (mora->owner == NULL)
 {
-free(perrito->name);
-free(perrito);
+free(mora->name);
+free(mora);
 return (NULL);
 }
 for (i = 0; i < n; i++)
-perrito->name[i] = name[i];
+mora->name[i] = name[i];
 
 for (j = 0; j < o + 1; j++)
-perrito->owner[j] = owner[j];
+mora->owner[j] = owner[j];
 
-perrito->age = age;
+mora->age = age;
 
-return (perrito);
+return (mora);
 }
 
