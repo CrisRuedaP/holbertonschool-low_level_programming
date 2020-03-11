2d565804ce89649e743b3ac986a3eb84382ff2bd
CrisRuedaP <crisruedap@gmail.com>
2020-03-11 21:38:19 +0000

    updated header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,8 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
+
+typedef unsigned long size_t; 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
