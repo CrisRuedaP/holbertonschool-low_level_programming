6435640101f2fa91fb7f28af31582ca1e093bb2e
CrisRuedaP <crisruedap@gmail.com>
2020-03-11 18:58:41 +0000

    Printed a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - prints a name.
+ * @name: parameter char type
+ * @f: function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+void (*pfn)(char *);
+pfn = f;
+pfn(name);
+}
