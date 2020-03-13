986582ed22a7552f8ec0230c4c934ebf6aa238b4
CrisRuedaP <crisruedap@gmail.com>
2020-03-13 02:43:06 +0000

    the function that selects the correct function to perform the operation asked by the user

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "3-calc.h"
 
 /**
- * get_op_func - function
+ * *get_op_func - function return a pointer to the operator given
  * @s: char
+ * Return: NULL
  */
 
 int (*get_op_func(char *s))(int, int)
+{
 op_t ops[] = {
 {"+", op_add},
 {"-", op_sub},
@@ -16,4 +19,16 @@ op_t ops[] = {
 {"%", op_mod},
 {NULL, NULL}
 };
-int i;
+int i = 0;
+
+while (ops[i].op)
+{
+if (strcmp(s, ops[i].op))
+{
+i++;
+}
+return (ops[i].f);
+}
+printf("Error\n");
+exit(99);
+}
