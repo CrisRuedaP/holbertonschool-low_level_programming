35bf48edbafc3d0b7862688d9f383bf90c94e84d
CrisRuedaP <crisruedap@gmail.com>
2020-03-13 02:41:25 +0000

    function main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,11 +1,31 @@
-#include <stdio.h>
 #include "3-calc.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - ppal function
+ * @argc: number of arguments
+ * @argv: positions of argumens
  * Return: void
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-return;
+int a, b, result;
+
+while (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+if (argv[2][1] != 0)
+printf("Error\n");
+exit(99);
+
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+result = get_op_func(argv[2])(a, b);
+
+printf("%d\n", result);
+return (0);
+
 }
