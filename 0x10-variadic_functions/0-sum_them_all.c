50e71382df6cd74c59bb37baf9a83984095fdfb8
CrisRuedaP <crisruedap@gmail.com>
2020-03-14 17:12:46 +0000

    fixed requeriment

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,17 +9,17 @@
 int sum_them_all(const unsigned int n, ...)
 {
 unsigned int i;
-int sum = 0;
+int sum;
 va_list arguments;
-
-va_start(arguments, n);
 
 if (n == 0)
 return (0);
 
+sum = 0;
+va_start(arguments, n);
 for (i = 0; i < n; i++)
 {
-sum += va_arg(arguments, unsigned int);
+sum += va_arg(arguments, int);
 }
 va_end(arguments);
 return (sum);
