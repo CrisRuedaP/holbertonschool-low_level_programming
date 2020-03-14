2bdd910f672e41f75bedea9754f0700e56a69159
CrisRuedaP <crisruedap@gmail.com>
2020-03-14 17:17:55 +0000

    fixed output

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,9 +13,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 unsigned int i;
 va_list args;
 
+/*
 if (separator == NULL)
 printf("\n");
-return;
+*/
 
 va_start(args, n);
 for (i = 0; i < n; i++)
