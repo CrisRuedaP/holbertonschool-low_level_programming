33adaedf0896aaabf08cdd1142ab7714f3988a57
CrisRuedaP <crisruedap@gmail.com>
2020-03-31 21:52:25 +0000

    header file

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h> 
+#include <sys/stat.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
