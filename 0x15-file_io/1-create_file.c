e080c0bafb6c06bba78b324725214001ff5caa0a
CrisRuedaP <crisruedap@gmail.com>
2020-04-01 01:04:29 +0000

    fixed output

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,6 +9,8 @@ int create_file(const char *filename, char *text_content)
 {
 int file, size, i = 0;
 
+if (text_content == NULL)
+text_content = "";
 
 file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (file == -1)
@@ -25,6 +27,5 @@ if (size == -1)
 return (-1);
 
 close(file);
-
 return (1);
 }
