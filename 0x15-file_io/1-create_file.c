3f9ba3a21078be7f665c6dd496f5d988a534ad34
CrisRuedaP <crisruedap@gmail.com>
2020-04-01 00:33:55 +0000

    fixed betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,11 +9,12 @@ int create_file(const char *filename, char *text_content)
 {
 int file, size, i = 0;
 
-if (filename == NULL)
-return (-1);
 
 file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (file == -1)
+return (-1);
+
+if (filename == NULL)
 return (-1);
 
 while (text_content[i] != '\0')
