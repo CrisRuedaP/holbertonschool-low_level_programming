c3696ecdc03e1f2290cd9ea25c21f090812c4f67
CrisRuedaP <crisruedap@gmail.com>
2020-04-02 01:07:15 +0000

    fixed output

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@ char buffer[1024];
 
 if (argc != 3)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+dprintf(STDERR_FILENO, "Usage: cp fd_from fd_to\n");
 exit(97);
 }
 fd_from = open(argv[1], O_RDONLY);
@@ -22,7 +22,7 @@ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 
-fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (fd_to == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
