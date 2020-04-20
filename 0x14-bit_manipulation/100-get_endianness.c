34dcffc73ba4627321635b6a862973689b470517
CrisRuedaP <crisruedap@gmail.com>
2020-04-20 00:32:48 +0000

    checked the endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * countSetBits - checks the endianness, describes the order in which a
+ * sequence of bytes are stored in computer memory. Can be either big or small.
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+char number;
+char *ptr;
+
+number = 1;
+ptr = &number;
+if (*ptr == 0)
+{
+return (0);
+}
+else
+return (1);
+}
