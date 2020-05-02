8a4823b46ade33dbf992e9a8c56a18736dfbde94
CrisRuedaP <crisruedap@gmail.com>
2020-05-02 16:13:55 +0000

    fixed betty

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,7 +3,7 @@
 /**
  * add_dnodeint - adds a new node at the beginning of a
  * @head: beginning of the list
- * @n:
+ * @n: new data
  * Return: the address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
@@ -18,6 +18,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->next = (*head);
 	(*head) = new_node;
 
-	return new_node;
+	return (new_node);
 
 }
